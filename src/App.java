7ec525597baf54f31aa0772ba439955b81c359c7
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-12-06 21:19:46 +0100

    done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] numero = new int[12];
+        int[] cuadrado = new int[12];
+        int[] cubo = new int[12];
+        int n = 12;
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%4d |", numero[i] = (int)(Math.random()*101));
+            System.out.printf(" %5d |", cuadrado[i] = numero[i] * numero[i]);
+            System.out.printf("%8d\n", cubo[i] = cuadrado[i] * numero[i]);
+        }
     }
 }
